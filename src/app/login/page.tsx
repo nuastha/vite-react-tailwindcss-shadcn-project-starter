dad19b9e52f13003f06826a36dfc5f51deeda075
nuastha <timilhllo@gmail.com>
2025-02-12 21:11:10 +0545

    add login page component with basic structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Page = () => {
+    return (
+        <div className="min-h-screen ">
+            <h1 className="text-3xl font-bold underline">
+                Login
+            </h1>
+        </div >
+    )
+}
+
+export default Page
